c833114b931e6acb92d6914efc00f57251a9c7f2
Mohamed.Moktar <mohamed.moktar@sqrm.net>
2022-11-26 12:12:43 +0200

    Initial

---
+++ SignalR/SignalR/HubConfig/MyHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.HubConfig
+{
+    public class MyHub:Hub
+    {
+        public async Task askServer(string message)
+        {
+            string tempString;
+            if(message == "hey")
+            {
+                tempString = "message was hey";
+            }
+            else
+            {
+                tempString = "message was somthing else ";
+            }
+            await Clients.Clients(this.Context.ConnectionId).SendAsync("askServerResponse",tempString);
+        }
+    }
+}
